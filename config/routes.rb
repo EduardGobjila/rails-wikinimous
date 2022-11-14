2c42deb0013248813d5bcca2014fb060bf039aa1
Eduard Gobjila <edikgobjila01@gmail.com>
2022-11-14 22:07:20 +0000

    trying to fix the error with the form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/article", to: "articles#index"
-  get "/article/:id", to: "articles#show"
+    resources :articles
+    root to: 'articles#index'
 end
