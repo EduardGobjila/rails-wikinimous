44d7e91a792f6e546ef7e499f3f33c469a38c074
Eduard Gobjila <edikgobjila01@gmail.com>
2022-11-15 09:23:31 +0000

    finished the crud with the article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,36 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new, stauts: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to invoices_path, notice: 'Post was successfully deleted.', status: :see_other
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
